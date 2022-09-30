f09b532ba069e3612fc996e74258cb495b13193b
Juan Carlos Nuñez <juancarlosm.nunez@gmail.com>
2022-09-30 15:51:22 -0300

    114. 10-show real info in the markup

--- src/components/PhotoItem/PhotoItem.js
+++ src/components/PhotoItem/PhotoItem.js
@@ -5,11 +5,13 @@ const PhotoItem = ({ photo }) => {
     return (  
         <div className="photo__item">
             <div className='photo__img--containter'>
-                <img className='photo__img' />
+                <img className='photo__img' src={photo.src.landscape} />
             </div>
             <div className='photo__buttons'>
-                <a href='' className='btn'></a>
-                <a href='' className='btn'></a> 
+                <a href={photo.photographer.url} className='btn'></a>
+                <a href={photo.url} className='btn'>
+                    view image
+                    </a> 
             </div>
 
         </div>
